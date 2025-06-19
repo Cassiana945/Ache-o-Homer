7cb3a0210819418c5c8d9e8b43896a4192da7547
Cassiana <160614428+Cassiana945@users.noreply.github.com>
2025-06-19 13:38:35 -0300

    Update Pontuacao.java

--- src/main/java/com/cassi/acheohomer/model/Pontuacao.java
+++ src/main/java/com/cassi/acheohomer/model/Pontuacao.java
@@ -1,4 +1,53 @@
 package com.cassi.acheohomer.model;
 
 public class Pontuacao {
+
+    private int id;
+    private String data;
+    private int pontos;
+    private boolean recorde;
+
+    public Pontuacao() {
+    }
+
+    public Pontuacao(int id, String data, int pontos, boolean recorde) {
+        this.id = id;
+        this.data = data;
+        this.pontos = pontos;
+        this.recorde = recorde;
+    }
+
+    public Pontuacao(String data, int pontos, boolean recorde) {
+        this.data = data;
+        this.pontos = pontos;
+        this.recorde = recorde;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
+
+    public boolean isRecorde() {
+        return recorde;
+    }
+
+    public void setRecorde(boolean recorde) {
+        this.recorde = recorde;
+    }
 }
